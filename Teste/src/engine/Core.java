2e2d4c5ee79454f9ad930f013c7cc65901fe32f8
Troloze <vitordilorenzzi1@gmail.com>
2022-03-25 15:25:32 -0300

    Meh

--- Teste/src/engine/Core.java
+++
@@ -1,8 +0,0 @@
-package engine;
-
-public class Core {
-	public static void main (String[] args) {
-		new MyFrame();
-	}
-	
-}
--- Teste/src/game/Entity.java
+++
@@ -1,6 +0,0 @@
-package game;
-
-public class Entity {
-	private int x, y;
-	private int speed;
-}
